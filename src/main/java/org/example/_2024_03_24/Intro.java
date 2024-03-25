55859efcd186b9509495d832a44a94d7931085a1
shonmikl <mikhailegorov676@gmail.com>
2024-03-25 12:35:29 +0100

    test commit

---
+++ src/main/java/org/example/_2024_03_24/Intro.java
@@ -0,0 +1,15 @@
+package org.example._2024_03_24;
+
+public class Intro {
+
+    public static int m1() {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int test = Intro.m1();
+
+        Intro intro = new Intro();
+
+    }
+}
