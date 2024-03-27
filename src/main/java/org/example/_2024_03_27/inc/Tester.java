75b8a6c0d405609493b44fc5c0a52c8894350d03
shonmikl <mikhailegorov676@gmail.com>
2024-03-27 11:42:11 +0100

    inc

---
+++ src/main/java/org/example/_2024_03_27/inc/Tester.java
@@ -0,0 +1,7 @@
+package org.example._2024_03_27.inc;
+
+public class Tester {
+    public static void main(String[] args) {
+        new User().getLogin();
+    }
+}
---
+++ src/main/java/org/example/_2024_03_27/inc/User.java
@@ -0,0 +1,13 @@
+package org.example._2024_03_27.inc;
+
+public class User {
+     private String login;
+
+     public String getLogin() {
+          return login;
+     }
+
+     public void setLogin(String login) {
+          this.login = login;
+     }
+}
