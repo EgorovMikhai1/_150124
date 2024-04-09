7cf02bef6eefddc0345d20a8a3d904ee62ad181b
shonmikl <mikhailegorov676@gmail.com>
2024-04-09 14:31:13 +0200

    matrix + hash

--- src/main/java/org/example/_2024_04_09/Matrix.java
+++ src/main/java/org/example/_2024_04_09/Matrix.java
@@ -2,9 +2,18 @@ package org.example._2024_04_09;
 
 public class Matrix {
 
-//    public int[][] getMatrix(int h, int l) {
-//
-//    }
+    //todo
+
+    /**
+     *
+     * @param matrix матрица в которой все числа отсортированы справа налево и сверху вниз
+     * @param number число которое ищем
+     * @return
+     */
+    public boolean isConsistNumber(int[][] matrix, int number) {
+        return true;
+    }
+
     public static void main(String[] args) {
 //        int[] array = {12, 34, 56, 7, 89};
 //
@@ -16,8 +25,8 @@ public class Matrix {
 
 
         int[][] matrix = {{1, 2, 3},
-                          {4, 5, 6},
-                          {7, 8, 9}};
+                {4, 5, 6},
+                {7, 8, 9}};
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
