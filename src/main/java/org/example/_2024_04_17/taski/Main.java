71bdd825ecf1b69f0a34693220d3403fa19cf991
shonmikl <mikhailegorov676@gmail.com>
2024-04-23 13:44:39 +0200

    task example

--- src/main/java/org/example/_2024_04_17/taski/Main.java
+++ src/main/java/org/example/_2024_04_17/taski/Main.java
@@ -1,11 +1,55 @@
 package org.example._2024_04_17.taski;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
+
+   // Find circles with radius equal to any triangle side:
+    public static Map<Circle, Triangle> findCirclesWithRadiusEqualToTriangleSide(List<Figure> figures) {
+        Map<Circle, Triangle> result = new HashMap<>();
+        List<Circle> circles = new ArrayList<>();
+        List<Triangle> triangles = new ArrayList<>();
+
+        for (Figure figure : figures) {
+            if (figure instanceof Circle) {
+                circles.add((Circle) figure);
+            }
+
+            if (figure instanceof Triangle) {
+                triangles.add((Triangle) figure);
+            }
+        }
+
+        for (Circle circle : circles) {
+            for (Triangle triangle : triangles) {
+                int a = triangle.getA();
+                int b = triangle.getB();
+                int c = triangle.getC();
+
+                int radius = circle.getRadius();
+
+                if(radius == a || radius == b || radius == c){
+                    result.put(circle, triangle);
+//                } else {
+//                    result.put(circle, false);
+                }
+            }
+        }
+        System.out.println(result.size());
+        return result;
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println(FigureGenerator.generateFigures());
+        System.out.println(findCirclesWithRadiusEqualToTriangleSide(FigureGenerator.generateFigures()));
     }
 }
 
@@ -16,6 +60,7 @@ class Figure {
 
 @ToString
 @AllArgsConstructor
+@Getter
 class Rectangle extends Figure {
     private Color color;
     private int a;
@@ -24,6 +69,7 @@ class Rectangle extends Figure {
 
 @ToString
 @AllArgsConstructor
+@Getter
 class Triangle extends Figure {
     private Color color;
     private int a;
@@ -33,6 +79,7 @@ class Triangle extends Figure {
 
 @ToString
 @AllArgsConstructor
+@Getter
 class Circle extends Figure {
     private Color color;
     private int radius;
