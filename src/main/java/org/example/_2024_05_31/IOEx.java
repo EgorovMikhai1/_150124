e9f6c0dc33e42aab24075c5f9b8542f6ef2c774e
shonmikl <mikhailegorov676@gmail.com>
2024-05-31 10:52:09 +0200

    tasks without mistakes

---
+++ src/main/java/org/example/_2024_05_31/IOEx.java
@@ -0,0 +1,31 @@
+package org.example._2024_05_31;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+//txt
+public class IOEx {
+    public static void main(String[] args) {
+        String text = "De conseguir el acceso a la misma, el Barça sólo podría jugar la Supercopa " +
+                "superando al Madrid en su serie semifinal o organizando la competición.";
+//        try {
+//            FileReader fileReader = new FileReader("222.txt");
+//            int character;
+//            while ((character = fileReader.read()) != -1) {
+//                System.out.print((char) character);
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+
+        try (FileWriter fileWriter = new FileWriter("write.txt", true)) {
+            for (int i = 0; i < text.length(); i++) {
+                fileWriter.write(text.charAt(i));
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
